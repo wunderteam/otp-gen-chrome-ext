f9ed201b102c862a8fc848915e0e9992c128f85b
Kevin Sweet <kevindsweet@gmail.com>
2020-03-10 14:02:31 -0600

    Trigger change event on input to ensure forms are updated

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,9 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     var textField = document.activeElement;
     textField.value = textField.value + message.key;
+
+    // Fire change event so controlled inputs know to update values
+    const changeEvent = document.createEvent("HTMLEvents");
+    changeEvent.initEvent("change", true, true);
+    textField.dispatchEvent(changeEvent);
 });
